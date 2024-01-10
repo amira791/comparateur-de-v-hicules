37f9bf8b5f68c8bdac6928a4bca1910a070c4818
amira791 <ka_bellali@esi.dz>
2024-01-10 17:39:17 +0100

    gestionVue

--- vue/adminVue/gestionVue.php
+++ vue/adminVue/gestionVue.php
@@ -18,6 +18,7 @@ class gestionVue {
     {
         ?>
         <link rel="stylesheet" type="text/css" href="../../styling/gestion.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
         <?php
     }
 
@@ -29,37 +30,51 @@ class gestionVue {
     }
 
 
-  
-
-    private function show_cadres ()
+    private function show_top_bar ()
     {
         ?>
-   <a href="../../router/adminRouter/gestionVehiculeRouter.php" class="cadre">
-        <h3>Gestion des « Véhicules »</h3>
-        <img src="../../images/gestion.png" alt="Image Cadre 1">
-    </a>
+       <img src="../../images/logo" id="logo">
+       <div class="top-bar"></div>
+       <?php
 
-    <a href="../../router/adminRouter/gestionAvisRouter.php" class="cadre">
-        <h3>Gestion des « Avis »</h3>
-        <img src="../../images/gestion.png" alt="Image Cadre 2">
-    </a>
+    }
 
-    <a href="../../router/adminRouter/gestionNewsRouter.php" class="cadre">
-        <h3>Gestion des « News »</h3>
-        <img src="../../images/gestion.png" alt="Image Cadre 3">
-    </a>
 
-    <a href="../../router/adminRouter/gestionUserRouter.php" class="cadre">
-        <h3>La gestion des utilisateurs</h3>
-        <img src="../../images/gestion.png" alt="Image Cadre 4">
-    </a>
+  
 
-    <a href="../../router/adminRouter/gestionParametreRouter.php" class="cadre">
-        <h3>Gestion des paramètres</h3>
-        <img src="../../images/gestion.png" alt="Image Cadre 5">
-    </a>
+    private function show_cadres()
+    {
+        ?>
+       <div class="top-bar"></div>
+        <div class="cadre-container">
+            <a href="../../router/adminRouter/gestionVehiculeRouter.php" class="cadre">
+                <h3>Gestion Véhicules </h3>
+                <img src="../../images/gestionveh.png" alt="Image Cadre 1">
+            </a>
+    
+            <a href="../../router/adminRouter/gestionAvisRouter.php" class="cadre">
+                <h3>Gestion Avis </h3>
+                <img src="../../images/gestionavii.webp" alt="Image Cadre 2">
+            </a>
+    
+            <a href="../../router/adminRouter/gestionNewsRouter.php" class="cadre">
+                <h3>Gestion News </h3>
+                <img src="../../images/news.webp" alt="Image Cadre 3">
+            </a>
+    
+            <a href="../../router/adminRouter/gestionUserRouter.php" class="cadre">
+                <h3>La gestion Users</h3>
+                <img src="../../images/gestionuser.png" alt="Image Cadre 4">
+            </a>
+    
+            <a href="../../router/adminRouter/gestionParametreRouter.php" class="cadre">
+                <h3>Gestion Paramètres</h3>
+                <img src="../../images/gestion.png" alt="Image Cadre 5">
+            </a>
+        </div>
         <?php
     }
+    
    
     public function Head_Page()
     {
@@ -73,8 +88,8 @@ class gestionVue {
     public function Body_Page()
     {
         echo '<body>';
+        $this->show_top_bar();
         $this->show_cadres();
-
         echo '</body>';
     }
 
